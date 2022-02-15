8eec2d675a9a2a55fb1b20819614011310450ced
C18384776 <C18384776@mytudublin.ie>
2022-02-15 12:16:14 +0000

    Group add,edit and remove working

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5.QtCore import QEvent, QEventLoop
-from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QInputDialog, QLineEdit
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QInputDialog, QLineEdit, QMessageBox
 
 from ui_main import *
 from registration import UiRegistration
@@ -92,7 +92,12 @@ class MainWindow(QMainWindow):
 
     def add_to_group(self):
         text, submit = QInputDialog.getText(self, "Add a new group", "Enter new group name")
-        if submit and text != '':
+
+        # Query checks if exact name already exists.
+        self.cur.execute("SELECT groupName FROM groups WHERE groupName = (?)", [text])
+        groups = self.cur.fetchall()
+        print("Inside group ADD: {}".format(groups))
+        if submit and text != '' and groups == []:
             self.ui.listWidget_groups.addItem(text)
             sql_group_insert = """INSERT INTO groups(groupName) VALUES(?)"""
             new_group_name = [text]
@@ -105,9 +110,34 @@ class MainWindow(QMainWindow):
         if group_to_edit is not None:
             text, submit = QInputDialog.getText(self, "Edit a group", "Edit group name", QLineEdit.Normal, group_to_edit)
 
-            if submit and text != '':
+            # Query checks if exact name already exists.
+            self.cur.execute("SELECT groupName FROM groups WHERE groupName = (?)", [text])
+            groups = self.cur.fetchall()
+            print("Inside group EDIT: {}".format(groups))
+
+            if submit and text != '' and groups == []:
                 edit_group_location.setText(text)
-                # self.ui.listWidget_groups.itemAt(edit_group_location).setText(text)
+                sql_group_update = """UPDATE groups SET groupName = (?) WHERE groupName = (?)"""
+                edit_group_name = [text, group_to_edit]
+                database.database_query(self.con, sql_group_update, edit_group_name)
+                self.con.commit()
+                self.reload_database()
+
+    def delete_group(self, delete_group_location, group_to_delete):
+        print("in delete group method with {}".format(group_to_delete))
+
+        reply = QMessageBox.question(self, "Remove a group",
+                                     "Do you really want to remove the group " + str(group_to_delete) + "?",
+                                     QMessageBox.Yes | QMessageBox.No)
+
+        if reply == QMessageBox.Yes and group_to_delete != "Recycle Bin":
+            row = self.ui.listWidget_groups.row(delete_group_location)
+            self.ui.listWidget_groups.takeItem(row)
+            sql_group_delete = """DELETE FROM groups WHERE groupName = (?)"""
+            delete_group_name = [group_to_delete]
+            database.database_query(self.con, sql_group_delete, delete_group_name)
+            self.con.commit()
+            self.reload_database()
 
 
     def eventFilter(self, source, event):
@@ -126,17 +156,13 @@ class MainWindow(QMainWindow):
 
                 if menu_select == new_group:
                     self.add_to_group()
-                    print(source.itemAt(event.pos()))
-                    print("new group")
                 elif menu_select == edit_group:
                     temp = source.itemAt(event.pos())
-                    print(temp)
                     self.edit_group(source.itemAt(event.pos()), temp.text())
-                    print(source.itemAt(event.pos()))
-                    print("edit group")
                 elif menu_select == delete_group:
+                    temp = source.itemAt(event.pos())
+                    self.delete_group(source.itemAt(event.pos()), temp.text())
                     print(source.itemAt(event.pos()))
-                    print("Delete group")
                 else:
                     print("user clicked out of group.")
 
@@ -150,8 +176,6 @@ class MainWindow(QMainWindow):
                 if menu_select == new_group:
                     self.add_to_group()
                     print(source.itemAt(event.pos()))
-                    print("User clicked on new group")
-                print("user clicked on non-group")
 
                 return True
 

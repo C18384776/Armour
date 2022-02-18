1854cf76970bd31196b72d304efb4a947b8540e7
C18384776 <C18384776@mytudublin.ie>
2022-02-18 20:47:44 +0000

    Refactoring QListWidget

---
+++ src/group_widget.py
@@ -0,0 +1,101 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QAction, QInputDialog, QLineEdit, QMessageBox
+import database
+
+
+def group_widget(source, event, cursor, list_widget, connection, main_window):
+    menu = QtWidgets.QMenu()
+    new_group_action = QAction("New Group")
+    edit_group_action = QAction("Edit Group")
+    delete_group_action = QAction("Delete Group")
+    menu.addAction(new_group_action)
+    menu.addAction(edit_group_action)
+    menu.addAction(delete_group_action)
+
+    # User right-clicks on a group.
+    if source.itemAt(event.pos()):
+        menu_select = menu.exec(event.globalPos())
+
+        if menu_select == new_group_action:
+            success = add_to_group(cursor, list_widget, connection, main_window)
+            return success
+        elif menu_select == edit_group_action:
+            temp = source.itemAt(event.pos())
+            success = edit_group(source.itemAt(event.pos()), temp.text(), main_window, cursor, connection)
+            return success
+        elif menu_select == delete_group_action:
+            temp = source.itemAt(event.pos())
+            delete_group(source.itemAt(event.pos()), temp.text(), main_window, list_widget, connection)
+            print(source.itemAt(event.pos()))
+        else:
+            print("user clicked out of group.")
+
+        return True
+    # User right clicks empty space.
+    else:
+        menu.clear()
+        menu.addAction(new_group_action)
+        menu_select = menu.exec(event.globalPos())
+
+        if menu_select == new_group_action:
+            success = add_to_group(cursor, list_widget, connection, main_window)
+            print(source.itemAt(event.pos()))
+            return success
+
+        return True
+
+
+def add_to_group(cursor, list_widget, connection, main_window):
+    text, submit = QInputDialog.getText(main_window, "Add a new group", "Enter new group name")
+
+    # Query checks if exact name already exists.
+    cursor.execute("SELECT groupName FROM groups WHERE groupName = (?)", [text])
+    groups = cursor.fetchall()
+    print("Inside group ADD: {}".format(groups))
+    if submit and text != '' and groups == []:
+        list_widget.addItem(text)
+        sql_group_insert = """INSERT INTO groups(groupName) VALUES(?)"""
+        new_group_name = [text]
+        database.database_query(connection, sql_group_insert, new_group_name)
+        connection.commit()
+        return True
+
+
+def edit_group(edit_group_location, group_to_edit, main_window, cursor, connection):
+    print("in edit group with {}".format(group_to_edit))
+    if group_to_edit is not None:
+        text, submit = QInputDialog.getText(main_window,
+                                            "Edit a group", "Edit group name",
+                                            QLineEdit.Normal,
+                                            group_to_edit)
+
+        # Query checks if exact name already exists.
+        cursor.execute("SELECT groupName FROM groups WHERE groupName = (?)", [text])
+        groups = cursor.fetchall()
+        print("Inside group EDIT: {}".format(groups))
+
+        if submit and text != '' and groups == []:
+            edit_group_location.setText(text)
+            sql_group_update = """UPDATE groups SET groupName = (?) WHERE groupName = (?)"""
+            edit_group_name = [text, group_to_edit]
+            database.database_query(connection, sql_group_update, edit_group_name)
+            connection.commit()
+            return True
+
+
+def delete_group(delete_group_location, group_to_delete, main_window, list_widget, connection):
+    print("in delete group method with {}".format(group_to_delete))
+
+    reply = QMessageBox.question(main_window, "Remove a group",
+                                 "Do you really want to remove the group " +
+                                 str(group_to_delete) +
+                                 "?", QMessageBox.Yes | QMessageBox.No)
+
+    if reply == QMessageBox.Yes and group_to_delete != "Recycle Bin":
+        row = list_widget.row(delete_group_location)
+        list_widget.takeItem(row)
+        sql_group_delete = """DELETE FROM groups WHERE groupName = (?)"""
+        delete_group_name = [group_to_delete]
+        database.database_query(connection, sql_group_delete, delete_group_name)
+        connection.commit()
+        return True
--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from ui_main import *
 from registration import UiRegistration
 from login import UiLogin
 import database
+import group_widget
 
 
 class MainWindow(QMainWindow):
@@ -125,59 +126,6 @@ class MainWindow(QMainWindow):
             print("database passed {}".format(self.current_database))
             self.reload_database()
 
-    def add_to_group(self):
-        text, submit = QInputDialog.getText(self, "Add a new group", "Enter new group name")
-
-        # Query checks if exact name already exists.
-        self.cur.execute("SELECT groupName FROM groups WHERE groupName = (?)", [text])
-        groups = self.cur.fetchall()
-        print("Inside group ADD: {}".format(groups))
-        if submit and text != '' and groups == []:
-            self.ui.listWidget_groups.addItem(text)
-            sql_group_insert = """INSERT INTO groups(groupName) VALUES(?)"""
-            new_group_name = [text]
-            database.database_query(self.con, sql_group_insert, new_group_name)
-            self.con.commit()
-            self.reload_database()
-
-    def edit_group(self, edit_group_location, group_to_edit):
-        print("in edit group with {}".format(group_to_edit))
-        if group_to_edit is not None:
-            text, submit = QInputDialog.getText(self,
-                                                "Edit a group", "Edit group name",
-                                                QLineEdit.Normal,
-                                                group_to_edit)
-
-            # Query checks if exact name already exists.
-            self.cur.execute("SELECT groupName FROM groups WHERE groupName = (?)", [text])
-            groups = self.cur.fetchall()
-            print("Inside group EDIT: {}".format(groups))
-
-            if submit and text != '' and groups == []:
-                edit_group_location.setText(text)
-                sql_group_update = """UPDATE groups SET groupName = (?) WHERE groupName = (?)"""
-                edit_group_name = [text, group_to_edit]
-                database.database_query(self.con, sql_group_update, edit_group_name)
-                self.con.commit()
-                self.reload_database()
-
-    def delete_group(self, delete_group_location, group_to_delete):
-        print("in delete group method with {}".format(group_to_delete))
-
-        reply = QMessageBox.question(self, "Remove a group",
-                                     "Do you really want to remove the group " +
-                                     str(group_to_delete) +
-                                     "?", QMessageBox.Yes | QMessageBox.No)
-
-        if reply == QMessageBox.Yes and group_to_delete != "Recycle Bin":
-            row = self.ui.listWidget_groups.row(delete_group_location)
-            self.ui.listWidget_groups.takeItem(row)
-            sql_group_delete = """DELETE FROM groups WHERE groupName = (?)"""
-            delete_group_name = [group_to_delete]
-            database.database_query(self.con, sql_group_delete, delete_group_name)
-            self.con.commit()
-            self.reload_database()
-
     def delete_entry(self, row_to_delete, id_of_entry, group_id_of_entry):
         print("in delete entry method with {} {} {}".format(row_to_delete, id_of_entry, group_id_of_entry))
 
@@ -273,43 +221,11 @@ class MainWindow(QMainWindow):
 
         # Event filter for QListWidget
         if event.type() == QEvent.ContextMenu and source is self.ui.listWidget_groups:
-            menu = QtWidgets.QMenu()
-            new_group = QAction("New Group")
-            edit_group = QAction("Edit Group")
-            delete_group = QAction("Delete Group")
-            menu.addAction(new_group)
-            menu.addAction(edit_group)
-            menu.addAction(delete_group)
-
-            # User right-clicks on a group.
-            if source.itemAt(event.pos()):
-                menu_select = menu.exec(event.globalPos())
-
-                if menu_select == new_group:
-                    self.add_to_group()
-                elif menu_select == edit_group:
-                    temp = source.itemAt(event.pos())
-                    self.edit_group(source.itemAt(event.pos()), temp.text())
-                elif menu_select == delete_group:
-                    temp = source.itemAt(event.pos())
-                    self.delete_group(source.itemAt(event.pos()), temp.text())
-                    print(source.itemAt(event.pos()))
-                else:
-                    print("user clicked out of group.")
-
-                return True
-            # User right clicks empty space.
+            success = group_widget.group_widget(source, event, self.cur, self.ui.listWidget_groups, self.con, self)
+            if success is True:
+                self.reload_database()
             else:
-                menu.clear()
-                menu.addAction(new_group)
-                menu_select = menu.exec(event.globalPos())
-
-                if menu_select == new_group:
-                    self.add_to_group()
-                    print(source.itemAt(event.pos()))
-
-                return True
-
+                print("Something in QListWidget went wrong.")
         return super(MainWindow, self).eventFilter(source, event)
 
 
@@ -323,7 +239,6 @@ class PasswordHide(QtWidgets.QStyledItemDelegate):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
